63c3ca517c4cb1d730ab22fc0a0136b700401e98
Arnav Kasturia <arnavkasturia@hotmail.com>
2023-02-12 22:44:36 -0500

    blue-gray about me

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -10,8 +10,13 @@ class AboutMe extends React.Component {
 
     render() {
         return(
-            <div class="tilt-div">
-                <p>Centered Text</p>
+            <div class="wrapper">
+                <div class="tilt-div">
+                    <p> It's me... your friendly neighborhood Software Engineer </p>
+                    <p>Hey! This is Arnav, I'm a Software Engineer at Credit Karma who enjoys building things.</p>
+                    <p>Look at the work experience section to learn more about my role at Credit Karma 
+                        and my past experiences.</p>
+                </div>
             </div>
         );
     }
